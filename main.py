868238f6275153a30c06b8e2c2d80ab746bfe0f6
Liam Colaluca <91563054+colalucal2024@users.noreply.github.com>
2021-09-30 18:41:21 +0000

    followed video

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from turtle import Screen
 from keyboardturtle import KeyboardTurtle
 from clickableturtle import ClickableTurtle
+from movingturtle import MovingTurtle
 
 
 # set up instance of the screen
 window = Screen()
-window.setup(800, 500)
+screen_width = 600
+screen_height = 400
+window.setup(screen_width, screen_height)
 
 # set up clickable instance
 button = ClickableTurtle()
 
 #set up players
 player_1 = KeyboardTurtle(window)
-player_2 = KeyboardTurtle(window, "w", "d")
+player_2 = KeyboardTurtle(window, "w","a", "s", "d")
 
 player_1.goto(100,0)
 
@@ -20,6 +23,7 @@ player_1.goto(100,0)
 player_1.other_player = player_2
 player_2.other_player = player_1
 
+moveT = MovingTurtle(screen_width)
 
 # This is needed to listen for inputs
 window.listen()
