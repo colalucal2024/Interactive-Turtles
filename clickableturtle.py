dd91686e95161f8964d3d41b96b96b23470d3bd0
Liam Colaluca <91563054+colalucal2024@users.noreply.github.com>
2021-10-05 19:07:56 +0000

    worked on game

--- clickableturtle.py
+++ clickableturtle.py
@@ -4,12 +4,11 @@ from random import choice
 class ClickableTurtle(Turtle):
   # our 'wrapper' class of the Turtle class
   def __init__(self, 
-               name = "button", 
-               x = 0 , 
-               y = -100):
+               name = "turtle", 
+               x = -190 , 
+               y = 160):
     # Runs Keyboard Turtle Constructor as well as the Turtle Constructor
     Turtle.__init__(self)
-    
     # Sets up incoming variables
     self.name = name
     self.x = x
@@ -17,8 +16,8 @@ class ClickableTurtle(Turtle):
     self.window = Screen()
 
     #set turtle starting states
-    self.shape("square")
-    self.shapesize(1,3,1)
+    self.shape("turtle")
+    self.shapesize(1,1,1)
     self.color(self.randcolor())
     self.penup()
     self.setx(self.x)
@@ -35,8 +34,7 @@ class ClickableTurtle(Turtle):
 
   # tells what happens when button is clicked
   def click(self, x, y):
-    # This is Placeholder:  What should this button do?
-    self.forward(100)
+   self.forward(100)
 
   # TODO:  
   # 1) Change the button color 
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from turtle import Screen
 from keyboardturtle import KeyboardTurtle
 from clickableturtle import ClickableTurtle
-from movingturtle import MovingTurtle
+from borders import BorderTurtle
+
+
+
 
 
 # set up instance of the screen
@@ -12,10 +15,11 @@ window.setup(screen_width, screen_height)
 
 # set up clickable instance
 button = ClickableTurtle()
+border = BorderTurtle()
 
-#set up players
+
 player_1 = KeyboardTurtle(window)
-player_2 = KeyboardTurtle(window, "w","a", "s", "d")
+player_2 = KeyboardTurtle(window, "w","a","s", "d")
 
 player_1.goto(100,0)
 
@@ -23,15 +27,8 @@ player_1.goto(100,0)
 player_1.other_player = player_2
 player_2.other_player = player_1
 
-moveT = MovingTurtle(screen_width)
+
 
 # This is needed to listen for inputs
 window.listen()
 window.mainloop()
-
-
-# be CAREFUL. We aren't controlling the screen draws in this program, so NO while True loops
-
-#TODO:  Check the classes and complete TODOs
-#push to github repo.
-#link repo to assignment
